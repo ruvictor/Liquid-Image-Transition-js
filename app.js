d36209cc7ae578577c02e8b94787b73fc68e897f
Victor <rusuvictor30@gmail.com>
2020-09-29 17:46:40 -0400

    Init

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent: document.querySelector('.app'),
+    intensity: 0.1,
+    image1: './image1.jpg',
+    image2: './image2.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
